9427e7c13e89e05b3a72a861065282ec784366e8
Hamza <6793siddique@gmail.com>
2024-09-13 11:11:06 +0500

    CS Fixer Setup

--- zoho-woocommerce-sync.php
+++ zoho-woocommerce-sync.php
@@ -1,44 +1,49 @@
 <?php
-/*
-Plugin Name: WooCommerce Zoho Sync
-Description: Sync WooCommerce products and orders with Zoho CRM.
-Version: 1.0
-Author: Hamza Siddique
+
+
+/**
+* Plugin Name:       WooCommerce Zoho Sync
+* Description:       Sync WooCommerce products and orders with Zoho CRM.
+* Version:           1.0.0
+* Author:            Hamza Siddique
+* Author URI:        https://www.upwork.com/freelancers/~01d452dc67bce01a15
+* License:           GPL-2.0+
+* License URI:       https://www.gnu.org/licenses/gpl-2.0.html
 */
 
-defined('ABSPATH') or die('No script kiddies please!');
 
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
+
+define( 'WZS_PREFIX', 'wzs_' );
 
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 
-require_once plugin_dir_path(__FILE__) . 'vendor/autoload.php';
+require_once plugin_dir_path( __FILE__ ) . 'vendor/autoload.php';
 
 
 // Include necessary files
-include_once plugin_dir_path(__FILE__) . 'wp/class/db-handler.php';
-include_once plugin_dir_path(__FILE__) . 'wp/class/product-sync.php';
-include_once plugin_dir_path(__FILE__) . 'wp/class/order-sync.php';
-include_once plugin_dir_path(__FILE__) . 'zoho/api-handler.php';
-include_once plugin_dir_path(__FILE__) . 'wp/class/cron-handler.php';
+require_once plugin_dir_path( __FILE__ ) . 'wp/class/db-handler.php';
+require_once plugin_dir_path( __FILE__ ) . 'wp/class/product-sync.php';
+require_once plugin_dir_path( __FILE__ ) . 'wp/class/order-sync.php';
+require_once plugin_dir_path( __FILE__ ) . 'zoho/api-handler.php';
+require_once plugin_dir_path( __FILE__ ) . 'wp/class/cron-handler.php';
 
 // Activation and Deactivation hooks
-register_activation_hook(__FILE__, 'zoho_woocommerce_sync_activate');
-register_deactivation_hook(__FILE__, 'zoho_woocommerce_sync_deactivate');
+register_activation_hook( __FILE__, 'zoho_woocommerce_sync_activate' );
+register_deactivation_hook( __FILE__, 'zoho_woocommerce_sync_deactivate' );
 
 // Plugin activation function
-function zoho_woocommerce_sync_activate()
-{
-    // Create necessary database tables
-    DB_Handler::create_tables();
-
-    // Schedule cron jobs
-    Cron_Handler::schedule_sync();
+function wzs_zoho_woocommerce_sync_activate() {
+	// Create necessary database tables
+	DB_Handler::create_tables();
+	// Schedule cron jobs
+	Cron_Handler::schedule_sync();
 }
 
 // Plugin deactivation function
-function zoho_woocommerce_sync_deactivate()
-{
-    // Remove scheduled cron jobs
-    Cron_Handler::unschedule_sync();
+function wzs_zoho_woocommerce_sync_deactivate() {
+	// Remove scheduled cron jobs
+	Cron_Handler::unschedule_sync();
 }
